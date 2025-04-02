3f29801118a959a05c9965c32875106b13c5ace4
snigdha-sukun <ssukun@infinitaslearning.com>
2025-04-02 10:16:39 +0200

    type error fix

--- src/app/api/collections/[collectionId]/photos/[photoId]/route.ts
+++ src/app/api/collections/[collectionId]/photos/[photoId]/route.ts
@@ -5,9 +5,10 @@ import dbConnect from "@/utils/MongodbConnect";
 
 export async function DELETE(
 	req: NextRequest,
-	{ params }: { params: { photoId: string; collectionId: string } },
+	context: { params: { photoId: string; collectionId: string } },
 ) {
-	const { photoId, collectionId } = params;
+	const { photoId, collectionId } = context.params;
+
 	await dbConnect();
 
 	const image = await ImageModel.findOne({ unsplashId: photoId }, "_id url");
